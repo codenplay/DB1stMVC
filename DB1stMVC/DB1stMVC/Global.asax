da9ac0150937bbc99e750bfa59e4c95b8439a0e6
codenplay <sr.hallow@gmail.com>
2015-09-16 00:11:36 +0600

    Project Up

---
+++ DB1stMVC/DB1stMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DB1stMVC.MvcApplication" Language="C#" %>
---
+++ DB1stMVC/DB1stMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DB1stMVC.Startup))]
+namespace DB1stMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
